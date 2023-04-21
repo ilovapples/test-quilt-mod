bf95e8154c8c15e26679a7d63de69902721c7fb7
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-21 14:10:00 -0500

    Update and rename src/main/kotlin/com/example/example_mod/ExampleMod.kt to src/main/kotlin/com/apples/test_quilt_mod/MainMod.kt

--- src/main/kotlin/com/apples/test_quilt_mod/MainMod.kt
+++ src/main/kotlin/com/apples/test_quilt_mod/MainMod.kt
@@ -1,11 +1,11 @@
-package com.example.example_mod
+package com.apples.test_quilt_mod
 
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object ExampleMod : ModInitializer {
+object MainMod : ModInitializer {
     val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
 
     override fun onInitialize(mod: ModContainer) {
